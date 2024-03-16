f6d838e7483b03c552cd0e1190ab6e3a70cd52d4
Rezwana Tasnin <110317053+rezwana-tasnin@users.noreply.github.com>
2024-03-16 19:44:22 +0600

    Added add_numbers

---
+++ src/add_numbers.py
@@ -0,0 +1,18 @@
+# def add_numbers():
+#     num1 = float(input('Enter number 1: '))
+#     num2 = float(input('Enter number 2: '))
+#     num3 = float(input('Enter number 3: '))
+#     num4 = float(input('Enter number 4: '))
+#     num5 = float(input('Enter number 5: '))
+#     return num1+num2+num3+num4+num5
+
+# sum = add_numbers()
+# print(sum)
+
+def add_numbers():
+    numbers = []
+    for index in range(5):
+        numbers.append(float(input(f'Enter number {index + 1}: ')))
+    return sum(numbers)
+
+print(f"The sum of numbers is: {add_numbers()}")
